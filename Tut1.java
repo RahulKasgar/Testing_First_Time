6e89c0dcd3c6dd37da1090e633109a9075f3a1de
Rahul Kasgar <37011738+RahulKasgar@users.noreply.github.com>
2018-03-03 15:04:17 +0530

    static ,instance and local variables

--- Tut1.java
+++ Tut1.java
@@ -2,23 +2,41 @@
 package tut1;
 
 public class Tut1 {
-    int length,breadth;
+   int x=10;
+    static int y=20;
     
-     void setDim(int l,int b)
+    
+     void show()
     {
-        l=length;
-        b=breadth;
+    int z=30;
+    System.out.println("instance variable"+x);
+    System.out.println("static variable"+y);
+    System.out.println("local variable"+z);
+    x++;
+    y++;
+    z++;
     }
-   
     
-    void showDim()
-    {
-    System.out.println("Length is"+length+"Breadth is"+breadth);    
-    } 
-   
     public static void main(String[] args) {  
         Tut1 t=new Tut1();
-        t.setDim(2,3);
-        t.showDim();       
+        Tut1 t1=new Tut1();
+         Tut1 t2=new Tut1();
+          Tut1 t3=new Tut1();
+           Tut1 t4=new Tut1();
+           t.show();
+           t1.show();
+           t2.show();
+           t3.show();
+           t4.show();
     }
 }
+////////////////////OUTPUT//////////////
+instance variable10
+static variable22
+local variable30
+instance variable10
+static variable23
+local variable30
+instance variable10
+static variable24
+local variable30
