6a67b6b705f62c4ae49c8a15110721fd8fe7ae2d
Rahul Kasgar <37011738+RahulKasgar@users.noreply.github.com>
2018-03-03 14:51:39 +0530

    classes concept

--- Tut1.java
+++ Tut1.java
@@ -2,69 +2,23 @@
 package tut1;
 
 public class Tut1 {
-    int l,b,h;
-    static int x=4;
+    int length,breadth;
     
-    
-    
-    
-    void showDim(int length,int breadth,int height)
+     void setDim(int l,int b)
     {
-    l=length;
-    b=breadth;
-    h=height;        
-    } 
+        l=length;
+        b=breadth;
+    }
    
     
-    
-    
-    static void mystati()
+    void showDim()
     {
-    System.out.print(x);
-    //System.out.print(l); error static variable cannot called in static method
-    
-    }
-     static
-    {
-    System.out.println("static block 2 ");
-    }
-    
-    
-     void show()
-    { 
-     System.out.println(l+" "+b+" "+h);
-     int a=4,c;
-     int b=6;
-     c=((a>b) ? a : b);
-     
-     System.out.println(+c+"static variable"+x);
-     
-    }
-     
-     //init blocks (instance initialization block) 
-     
-      {
-     System.out.println("initialization block ");
-        
-        }
-     
-      
-      //static initialization block
-     static
-    {
-    System.out.println("static block1 ");
-    }
-     
-    
+    System.out.println("Length is"+length+"Breadth is"+breadth);    
+    } 
+   
     public static void main(String[] args) {  
         Tut1 t=new Tut1();
-        
-       
-        t.showDim(2, 4, 6);
-        t=new Tut1();
-        t.show();
-        
-        
-        
+        t.setDim(2,3);
+        t.showDim();       
     }
 }
